a7e5007ac31a8d90c9c2d17af908a58d42a436c7
milexm <milexm@gmail.com>
2022-10-18 10:59:57 -0700

    Repo reorg

---
+++ code-examples/basic-examples/glossary-samples/README.md
@@ -0,0 +1,7 @@
+---
+last update: 10/15/22
+---
+
+# Glossary realted semples
+
+Examples supporting glossary definitiions. 
---
+++ code-examples/basic-examples/glossary-samples/package-init/pkg/string_to_lower.py
@@ -0,0 +1,7 @@
+"""
+Simple module to show the use of `__init__.py in packages. 
+"""
+
+# Return lowercase string.
+def stringToLower(inStr):
+    return inStr.lower()
