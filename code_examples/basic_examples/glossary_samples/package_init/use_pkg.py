3ddd58f2cba507496e1f86e80971d6b801dbd6d4
milexm <milexm@gmail.com>
2022-10-22 12:38:42 -0700

    Update use_pkg.py

--- code_examples/basic_examples/glossary_samples/package_init/use_pkg.py
+++ code_examples/basic_examples/glossary_samples/package_init/use_pkg.py
@@ -18,8 +18,21 @@ import mypkg
 some_string = "Hello, Universe!"
 
 
-" The imports defined in [__intit__.py](./code_examples/packages/mypkg/__init__.py), allow the use of functions from  different modules as they belonged to one package only, without the need to import the various modules one by one. "
+def handle_string():
 
-print("Lower case: " + mypkg.stringToLower(some_string))
-print("Upper case: " + mypkg.stringToUpper(some_string))
-print("String length: " + str(mypkg.stringLength(some_string)))
+    """ The imports defined in [__intit__.py](./code_examples/packages/mypkg/__init__.py), allow the use of functions from  different modules as they belonged to one package only, without the need to import the various modules one by one. """
+
+    print("Lower case: " + mypkg.stringToLower(some_string))
+    print("Upper case: " + mypkg.stringToUpper(some_string))
+    print("String length: " + str(mypkg.stringLength(some_string)))
+
+
+def main():
+    """
+        Displays menu and processes user's input.
+        It calls the proper method based on the user's selection.
+    """
+    handle_string()
+
+if __name__ == '__main__':
+    main()
