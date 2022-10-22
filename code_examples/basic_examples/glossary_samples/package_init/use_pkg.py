aad16a402c06b1353b6e691fd7000acfd861e564
milexm <milexm@gmail.com>
2022-10-22 12:13:52 -0700

    Fixed comments

--- code_examples/basic_examples/glossary_samples/package_init/use_pkg.py
+++ code_examples/basic_examples/glossary_samples/package_init/use_pkg.py
@@ -1,22 +1,25 @@
 """
-Show how the use of `__init__.py in packages. 
+Shows how the use a package.
+
+Imports a package and uses the functions defined in the modules contained in the package. 
 """
 
 import sys
   
 """ 
 Append the path to the packages location.
-This is important to direct the interpreter where to find the packages.
+This is important to allow the interpreter where to find the packages.
 """
-
 sys.path.append('./code_examples/packages')
 
-import pkg
+import mypkg
 
+""" Text to handle. """
 some_string = "Hello, Universe!"
 
-" The imports defined in __intit__.py, allow the use of functions from  different modules as they belonged to one package only, without the need to import the various modules one by one. "
 
-print("Lower case: " + pkg.stringToLower(some_string))
-print("Upper case: " + pkg.stringToUpper(some_string))
-print("String length: " + str(pkg.stringLength(some_string)))
+" The imports defined in [__intit__.py](./code_examples/packages/mypkg/__init__.py), allow the use of functions from  different modules as they belonged to one package only, without the need to import the various modules one by one. "
+
+print("Lower case: " + mypkg.stringToLower(some_string))
+print("Upper case: " + mypkg.stringToUpper(some_string))
+print("String length: " + str(mypkg.stringLength(some_string)))
