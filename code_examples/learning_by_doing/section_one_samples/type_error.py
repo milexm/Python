3a18c62047df884f3b34b227d49e582b3c4d9192
milexm <milexm@gmail.com>
2022-11-28 15:26:11 -0800

    section-one-samples-comments-format (#41)
    * Added new function create_list_of_numbers
    
    * recreated ref docs
    
    * Created two new functions: create_number_list_in_range and create_string_list_in_range. Fixed comments.
    
    * Fixed comments format

--- code_examples/learning_by_doing/section_one_samples/type_error.py
+++ code_examples/learning_by_doing/section_one_samples/type_error.py
@@ -11,12 +11,15 @@ def issue_type_error():
     Remarks
     -------
 
-    Python can be of has different types. In this example, the value assigned to `a`  is of string type (i.e., text) while the value of `b` is an integer (i.e., whole number),
+    Python can be of has different types. In this example, the value assigned to
+    `a`  is of string type (i.e., text) while the value of `b` is an integer
+    (i.e., whole number),
 
     You cannot add strings with integers, otherwise you get the following error:
     `TypeError: can only concatenate str (not "int") to str`.
     
-    Therefore, you need to convert the string to an integer using the `int()` built-in function. The correct statement is `print(int(a) + b)`.
+    Therefore, you need to convert the string to an integer using the `int()`
+    built-in function. The correct statement is `print(int(a) + b)`.
 
     """
     a = "1" # This is a string.
