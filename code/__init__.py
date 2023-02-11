b86f398b2fd8f6d956e22cc2998633ab04f6fadb
milexm <milexm@gmail.com>
2023-02-10 17:21:06 -0800

    Update ref docs

---
+++ code/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. include:: ./README.md
+"""
+
+"""
+The landing page for your documentation is your project's top-level
+<modulename>/__init__.py file. Adding a module-level docstring here is a great
+way to introduce users to your project. For example, the documentation you are
+reading right now is sourced from pdoc/__init__.py. You can also include your
+title page from a Markdown file.
+
+This __init__.py is important to generate the API reference docimentation using `pdoc`. We trick pdoc pretending that the `code` folder is a package.
+"""
+
