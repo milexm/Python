d0778541530f78ef15eeee3197edf2795aafb021
milexm <milexm@gmail.com>
2023-02-12 19:22:30 -0800

    Update __init__.py

--- code/__init__.py
+++ code/__init__.py
@@ -3,7 +3,7 @@
 """
 
 """
-DO NOT DLETE THIS FILE! 
+DO NOT DELETE THIS FILE! 
 
 This __init__.py is important to generate the API reference documentation using `pdoc`. We trick pdoc pretending that the `code` folder is a package. 
 """
