5e8580d17aa1e1b546eab933a0ffad8fef065e96
milexm <milexm@gmail.com>
2023-07-27 17:52:03 -0700

    Fixed comment typos.

--- code/menus_creation/main.py
+++ code/menus_creation/main.py
@@ -30,7 +30,7 @@ sys.path.append('./code/menus_creation/plotting_away')
 from my_plot import PlotSamples 
 
 class SubMenus(ConsoleMenu):
-    """ 
+    """
     The class `SubMenus` creates the various group menus (submenus).  The class
     instance method `group_selection_submenu` displays a menu of available
     samples, for the user's selected group, and allows the user to select a
@@ -39,30 +39,31 @@ class SubMenus(ConsoleMenu):
 
     def __init__(self):
         ''' Initialize menu choices for each sample group and group them in the
-            `sub_menus` list attribute. Instanciate each sample class. 
+            `sub_menus` list attribute. Instantiate each sample class.
             Define the decision table for each sample group.  
-            Group all the samples decision table. 
+            Group all the sample decision tables.
         '''
 
         self.number_menu_choices = ["Fibonacci", "Numbers", "Quit"]
         """ Choices for the Numbers group menu"""
-        
+       
         self.plot_menu_choices = ["Plot", "Quit"]
         """ Choices for the Plot group menu"""
 
-        # The order must match the order of the `self.menu_items` 
+        # The order must match the order of the `self.menu_items`
         # list in `code/menus_creation/main.py`.  
         self.sub_menus = [
             [], # Leave it empty to match dictionary keys.
                 # This is because the start key is 1 in the related
-                # selection table (dictionary) `sub_menu` defined 
-                # in main.py.   
-            self.number_menu_choices, # Value associated with key 1 
-            self.plot_menu_choices    # Value associated with key 2 
+                # selection table (dictionary) `sub_menu` defined
+                # in main.py.  
+            self.number_menu_choices, # Value associated with key 1
+            self.plot_menu_choices    # Value associated with key 2
         ]
         """ Group of all the sample menus. """
 
-        # Instanciate each sample class. 
+
+        # Instantiate each sample class.
 
         self.number_samples_instance = NumberSamples()
         """ `NumberSamples` instance. """
@@ -71,10 +72,10 @@ class SubMenus(ConsoleMenu):
         """ `PlotSamples` instance. """
 
         # Define the decision table for each sample group.  
-        # Each table entry contain the name of the sample and 
-        # the method to call. 
-        # Note the use of the `lambda' function to pass 
-        # parameters to the function to call, when needed. 
+        # Each table entry contain the name of the sample and
+        # the method to call.
+        # Note the use of the `lambda' function to pass
+        # parameters to the function to call, when needed.
 
         self.number_samples = {
             1: ["\n***  Calculate Fibonacci ***", lambda: self.number_samples_instance.fiboTriangle(5)],
@@ -87,12 +88,12 @@ class SubMenus(ConsoleMenu):
         }
         """ Sub menu Plotting selection decision table. """
 
-    
         self.sample_groups = {
             1: ["Numbers Samples", self.number_samples],
             2: ["Plot Samples", self.plot_samples]
         }
-        """ Groups of all the samples decision table. """
+        """ Groups of all the sample decision tables. """
+
 
     def group_selection_submenu(self, sub_menu):
         """
@@ -108,6 +109,7 @@ class SubMenus(ConsoleMenu):
         # Initialize selected menu name and items through the `ConsoleMenu` parent class.  
         super().__init__(selected_menu_name, selected_sub_menu_items)
 
+
         while True:
 
             # Display the menu.
@@ -119,21 +121,24 @@ class SubMenus(ConsoleMenu):
             if choice == len(selected_sub_menu_items):
                 break        
             else:
-                print(f"\n*** Sample results ***") 
-                
-                # Get the selected list (second dictionary elememnt)
-                _current_selection = self.sample_groups[sub_menu][1] 
-                
-                # Call the selectd sample function (second list element). 
+                print(f"\n*** Sample results ***")
+               
+                # Get the selected list (second dictionary element)
+                _current_selection = self.sample_groups[sub_menu][1]
+               
+                # Call the selected sample function (second list element).
                 _current_selection[int(choice)][1]()
+
 
 class MainMenu(ConsoleMenu):
 
-    """ 
+
+    """
     The `MainMenu` class allows the user to select a group of examples to
     execute. It initializes the instance of the class by defining a list of menu
-    choices. It also instantiates and initialiazes an instance of the
+    choices. It also instantiates and initializes an instance of the
     `ConsoleMenu` class.
+
 
     Remarks
     -------
@@ -144,6 +149,7 @@ class MainMenu(ConsoleMenu):
     instantiate an instance of the `_menu.list_menu` class called `_lmenu` and
     then call the `list_selection_menu` method on `_lmenu`.
 
+
     """
 
     def __init__(self):
@@ -151,34 +157,37 @@ class MainMenu(ConsoleMenu):
         choices. Initialize the main menu name and menu choices via the
         `ConsoleMenu` parent class. Define the decision table to select the submenus. """
 
-        # Define the choices of the mzin menu. Every choice
-        # represents a group of samplea. 
+
+        # Define the choices of the main menu. Every choice
+        # represents a group of samples.
         self.menu_choices = ["Numbers", "Plotting", "Quit"]
         """ Choices for the Main Group menu"""
-    
-        # Initialize menu name and the menu choices via 
+   
+        # Initialize menu name and the menu choices via
         # the `ConsoleMenu` parent class.  
         super().__init__("Main Menu", self.menu_choices)
 
 
-       # Instantiate the sub menus class. 
+       # Instantiate the sub menus class.
         _submenus = SubMenus()
         """ Sub menu class instance. """
 
-        # Define the decision table to select the submenus. 
-        # The order must match the order of the `self.sub_menus` 
+
+        # Define the decision table to select the submenus.
+        # The order must match the order of the `self.sub_menus`
         # list in `code/menus_creation/submenus.py`.  
         self.sub_menu = {
             1:  lambda: _submenus.group_selection_submenu(1), # Numbers
-            2:  lambda: _submenus.group_selection_submenu(2), # Plotting 
+            2:  lambda: _submenus.group_selection_submenu(2), # Plotting
         }
         """ Sub menu selection decision table. """
 
+
     def group_selection_menu(self):
         """
-        Display the group menu and and start an endless loop.  Wait for the
+        Display the group menu and start an endless loop.  Wait for the
         user's input and display the submenu based on the user's selection.  
-        End the loop when the user enters the`Quit` request. 
+        End the loop when the user enters the`Quit` request.
         """
 
         while True:
@@ -190,12 +199,12 @@ class MainMenu(ConsoleMenu):
             choice = self.get_user_choice()
 
             if choice == len(self.menu_choices):
-                # User selected `Quit`. 
+                # User selected `Quit`.
                 break        
             else:
-                # Display the selected sub menu. 
+                # Display the selected sub menu.
                 self.sub_menu[choice]()
-            
+ 
 
 if __name__ == '__main__':
     # Instantiate the class.
