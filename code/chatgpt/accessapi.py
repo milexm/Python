68c1a121347ce87cd36a2cf78954d1c6b639b7bd
milexm <milexm@gmail.com>
2023-08-02 07:43:34 -0700

    Update accessapi.py

--- code/chatgpt/accessapi.py
+++ code/chatgpt/accessapi.py
@@ -1,12 +1,27 @@
+""" 
+Module accessapi.py
+"""
 
 import openai
 
 def simple_question():
+    """
+        Aask a simple question to ChatGPT. 
+
+        Remarks
+        -------
+        This simple function tests the enviroment settings to 
+        determine if the infrastructure is in place to communicate
+        with ChatGPT via the OpenAI API.  
+     """
+
 
     secret_key = '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'
     openai.api_key = secret_key
 
+    # Simple prompt or question to ask. 
     my_prompt = 'Tell me a short ad for a homme security company'
+
 
     output = openai.Completion.create(
         model = 'text-davinci-003',
